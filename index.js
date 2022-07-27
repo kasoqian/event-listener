3192656f92ff04781a8470e0b949eaf7a01f1711
kasoqian <95743792+kasoqian@users.noreply.github.com>
2022-07-27 18:45:30 +0800

    chore: 删除无用代码

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 import { ethers, Contract, utils } from "ethers";
 import { usdtABI, usdtAddress } from "./coins/usdt.js";
 
-/* (async () => {
-  const a = await provider.getNetwork();
-  console.log(a);
-})(); */
-
 function main() {
   const provider = new ethers.getDefaultProvider("mainnet", {
     etherscan: process.env.EVM_KEY,
@@ -20,19 +15,3 @@ function main() {
 }
 
 main();
-
-/* const topicSets = [
-  utils.id(
-    "Transfer(address indexed from, address indexed to, uint256 indexed tokenId)",
-  ),
-  null,
-  null,
-];
-
-const contract = new Contract(tokenAddress, abi, provider);
-
-// List all token transfers *from* myAddress
-/* contract.filters.Transfer(null); */
-/* await provider.on("data", tx => {
-  console.log(tx);
-}); */
